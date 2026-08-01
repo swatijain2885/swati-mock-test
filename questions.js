40211d2079b516181c58beb933d1e61a1db5ce5f
swatijain2885 <swatijain2885@gmail.com>
2026-08-01 13:11:23 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,33 @@
+const questions = [
 
+{
+question:"Which function is used to search data in Excel?",
+options:["SUM","COUNT","XLOOKUP","MAX"],
+answer:"XLOOKUP"
+},
+
+{
+question:"Which function is used to add numbers?",
+options:["SUM","AVERAGE","MAX","COUNT"],
+answer:"SUM"
+},
+
+{
+question:"Which function counts numeric cells?",
+options:["COUNT","COUNTA","IF","SUM"],
+answer:"COUNT"
+},
+
+{
+question:"Which chart is best for percentage?",
+options:["Bar","Pie","Line","Area"],
+answer:"Pie"
+},
+
+{
+question:"Shortcut key for Save?",
+options:["Ctrl+S","Ctrl+C","Ctrl+V","Ctrl+Z"],
+answer:"Ctrl+S"
+}
+
+];
