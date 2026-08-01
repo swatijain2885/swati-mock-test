8694b6220cb10f5d8f7ce4d458736d7e2d85f2ca
swatijain2885 <swatijain2885@gmail.com>
2026-08-01 13:17:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+let currentQuestion = 0;
 
+const questionText = document.querySelector("p");
+const options = document.querySelectorAll("input[type='radio']");
+const labels = ["SUM","COUNT","XLOOKUP","MAX"];
+const nextButton = document.querySelector("button");
+
+function loadQuestion() {
+
+    questionText.innerHTML = questions[currentQuestion].question;
+
+    for(let i=0;i<4;i++){
+        options[i].nextSibling.textContent=" "+questions[currentQuestion].options[i]+" ";
+        options[i].checked=false;
+    }
+
+}
+
+nextButton.addEventListener("click",function(){
+
+    if(currentQuestion<questions.length-1){
+
+        currentQuestion++;
+
+        loadQuestion();
+
+    }else{
+
+        alert("Mock Test Completed");
+
+    }
+
+});
+
+loadQuestion();
